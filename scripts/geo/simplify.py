1cdecc0c781240e5bc59530973ff09be4a868e5b
Philip <philip@northarrowresearch.com>
2025-07-24 12:49:22 -0700

    tweaking simplify script

--- scripts/geo/simplify.py
+++ scripts/geo/simplify.py
@@ -1,3 +1,5 @@
+# https://geopandas.org/en/latest/docs/reference/api/geopandas.GeoSeries.simplify_coverage.html
+
 # import geopandas as gpd
 
 # # Load layer
@@ -23,10 +25,13 @@ print(gpd.__version__)
 print(shapely.__version__)
 
 # Read the GeoPackage
-gdf = gpd.read_file("/Users/philipbailey/GISData/watershed_boundaries/simplification_experiment/huc10_conus_single_5070.gpkg")
+gdf = gpd.read_file("/Users/philipbailey/GISData/riverscapes/rme/Metric_Engine-Metric_Engine_for_Comb_Wash-San_Juan_River/inputs/inputs.gpkg", layer='vbet_dgos')
+
+# Reproject to EPSG:5070
+gdf = gdf.to_crs(epsg=5070)
 
 # Simplify all geometries using simplify_coverage on the GeoSeries
-gdf["geometry"] = gdf.geometry.simplify_coverage(tolerance=10000)
+gdf["geometry"] = gdf.geometry.simplify_coverage(tolerance=11)
 
 # Save the result
-gdf.to_file("/Users/philipbailey/GISData/watershed_boundaries/simplification_experiment/simplified_layer2.gpkg", driver="GPKG")
+gdf.to_file("/Users/philipbailey/GISData/temp/simple_vbet_dgos_11.gpkg", driver="GPKG")
