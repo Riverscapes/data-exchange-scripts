8b0ab9effe492c4c77b5590c24d4318cccf197ad
Lorin Gaertner <lorin@northarrowresearch.com>
2026-01-27 12:19:02 -0800

    progress

--- scripts/utility/project_attribution/apply_attribution.py
+++ scripts/utility/project_attribution/apply_attribution.py
@@ -1,6 +1,14 @@
 """Bulk apply project attribution to projects in Data Exchange
 issue https://github.com/Riverscapes/rs-web-monorepo/issues/861 
 
+A project's attribution consists of a list of attribution objects, 
+each of which is an Organization and a list of Roles from the AttributionRoleEnum
+
+There are three ways someone might want to change attribution:
+1. add (do not change existing, apply new on top of it)
+2. replace (remove any existing attribution and apply new)
+3. remove (remove specific attribution but leave all others in place)
+
 Lorin Gaertner
 January 2026
 """
@@ -10,18 +18,33 @@ from rsxml import ProgressBar, dotenv, Logger
 from pydex import RiverscapesAPI
 
 
-def build_attribution_params():
+def build_attribution_params() -> tuple[list[str], str, list[str]]:
     """Assemble:
     * list of projects IDs to apply attribution to
-    * ProjectAttribution Object (Organization ID, list of AttributionRoleEnum)
+    * ProjectAttribution Object Organization ID
+    * ProjectAttribution Object list of AttributionRoleEnum
     """
     return (['73cc1ada-c82b-499e-b3b2-5dc70393e340'], 'cc4fff44-f470-4f4f-ada2-99f741d56b28', ['CO_FUNDER', 'DESIGNER'])
 
 
 def apply_attribution(api: RiverscapesAPI, stage: str, attribution_params: tuple[list[str], str, list[str]]):
+    # Project.attribution is an array of [ProjectAttribution!]!
     # ProjectAttribution is organization: Organization! , role [AttributionRoleEnum!]
-    # Project.attribution is an array of [ProjectATtribution!]!
-    print("hello")
+    log = Logger('Apply attribution')
+    log.title('Apply attribution')
+    mutation = api.load_mutation('updateProject')
+    project_ids, org_id, roles = attribution_params
+
+    attribution_item = {
+        "organizationId": org_id,
+        "roles": roles
+    }
+
+    prg = ProgressBar(total=len(project_ids), text='Attributing projects')
+    for i, project_id in enumerate(project_ids):
+        variables = {
+            "projectId": project_id,
+        }
 
 
 if __name__ == '__main__':
