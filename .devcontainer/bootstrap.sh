56d8ed3172cb41822263132fdfa6ca64e61e8c0e
Matt <1063391+MattReimer@users.noreply.github.com>
2026-03-25 19:04:15 +0000

    codespace fixes

--- .devcontainer/bootstrap.sh
+++ .devcontainer/bootstrap.sh
@@ -10,5 +10,11 @@ uv sync
 # The Python gdal version must match the system libgdal version, so it cannot
 # be pinned in pyproject.toml generically. We install it here explicitly.
 sudo apt-get update
-sudo apt-get install -y --no-install-recommends libgdal-dev gdal-bin
+sudo apt-get install -y --no-install-recommends libgdal-dev gdal-bin libsqlite3-mod-spatialite unzip
 bash install_geo.sh
+
+# Install AWS CLI v2
+curl -fsSL "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
+unzip -q /tmp/awscliv2.zip -d /tmp
+sudo /tmp/aws/install
+rm -rf /tmp/awscliv2.zip /tmp/aws
